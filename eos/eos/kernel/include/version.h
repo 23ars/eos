d7fdcf908124658b4652b442dad8b336418707f8
23ars <ardeleanasm@gmail.com>
2016-03-06 18:32:55 +0200

    Add new project structure #30

---
+++ eos/eos/kernel/include/version.h
@@ -0,0 +1,17 @@
+/*
+ * version.h
+ *
+ * Created: 2/7/2016 12:06:51 AM
+ *  Author: Mihai
+ */ 
+
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define MAJOR_NUMBER 0
+#define MINOR_NUMBER 0
+#define BUILD_NUMBER 1
+
+
+#endif /* VERSION_H_ */
